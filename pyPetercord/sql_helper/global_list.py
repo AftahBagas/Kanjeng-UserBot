71c7157e06a2acb3c879c4afc053c79a8cf237f3
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-09 23:49:27 +0700

    Update global_list.py

--- pyPetercord/sql_helper/global_list.py
+++ pyPetercord/sql_helper/global_list.py
@@ -19,7 +19,7 @@ class PetercordGloballist(BASE):
 
     def __eq__(self, other):
         return bool(
-            isinstance(other, CatGloballist)
+            isinstance(other, PetercordGloballist)
             and self.keywoard == other.keywoard
             and self.group_id == other.group_id
         )
