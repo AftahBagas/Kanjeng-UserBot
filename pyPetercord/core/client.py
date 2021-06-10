302b170df64b7c1679b4d41b2e9a79f877fbccd2
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-10 08:52:21 +0700

    Update client.py

--- pyPetercord/core/client.py
+++ pyPetercord/core/client.py
@@ -37,7 +37,7 @@ sudo_enabledcmds = sudo_enabled_cmds()
 
 
 class PetercordBotClient(TelegramClient):
-    def cat_cmd(
+    def petercord_cmd(
         self: TelegramClient,
         pattern: str or tuple = None,
         info: Union[str, Dict[str, Union[str, List[str], Dict[str, str]]]]
