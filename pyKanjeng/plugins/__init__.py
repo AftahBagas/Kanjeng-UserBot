2347060b04307e5f75a2d2c1079b09ba4bcbf106
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-11 09:43:17 +0700

    Update and rename pyPetercord/plugins/__init__.py to pyKanjeng/plugins/__init__.py

--- pyKanjeng/plugins/__init__.py
+++ pyKanjeng/plugins/__init__.py
@@ -13,20 +13,20 @@ from .. import *
 from ..Config import Config
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
-from ..core.session import petercord
+from ..core.session import kanjeng
 from ..helpers import *
 from ..helpers.utils import (
     _format,
-    _petercordtools,
-    _petercordutils,
+    _kanjengtools,
+    _kanjengutils,
     install_pip,
     reply_id,
 )
 
 # =================== CONSTANT ===================
-bot = petercord
+bot = kanjeng
 LOGS = logging.getLogger(__name__)
-USERID = petercord.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
+USERID = kanjeng.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
 ALIVE_NAME = Config.ALIVE_NAME
 AUTONAME = Config.AUTONAME
 DEFAULT_BIO = Config.DEFAULT_BIO
@@ -39,7 +39,7 @@ HEROKU_API_KEY = Config.HEROKU_API_KEY
 
 thumb_image_path = os.path.join(Config.TMP_DOWNLOAD_DIRECTORY, "thumb_image.jpg")
 
-USERID = petercord.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
+USERID = kanjeng.uid if Config.OWNER_ID == 0 else Config.OWNER_ID
 
 # mention user
 mention = f"[{Config.ALIVE_NAME}](tg://user?id={USERID})"
