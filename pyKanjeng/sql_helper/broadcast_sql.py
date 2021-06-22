7c94bbcf6200ed427191727b3cc627f220f20acc
Kanjeng Ingsun <aftahbagas21@gmail.com>
2021-06-22 23:32:43 +0000

    auto back: code format

--- pyKanjeng/sql_helper/broadcast_sql.py
+++ pyKanjeng/sql_helper/broadcast_sql.py
@@ -52,9 +52,7 @@ def add_to_broadcastlist(keywoard, group_id):
 
 def rm_from_broadcastlist(keywoard, group_id):
     with KANJENGBROADCAST_INSERTION_LOCK:
-        broadcast_group = SESSION.query(KanjengBroadcast).get(
-            (keywoard, str(group_id))
-        )
+        broadcast_group = SESSION.query(KanjengBroadcast).get((keywoard, str(group_id)))
         if broadcast_group:
             if str(group_id) in BROADCAST_SQL_.BROADCAST_CHANNELS.get(keywoard, set()):
                 BROADCAST_SQL_.BROADCAST_CHANNELS.get(keywoard, set()).remove(
@@ -71,9 +69,7 @@ def rm_from_broadcastlist(keywoard, group_id):
 
 def is_in_broadcastlist(keywoard, group_id):
     with KANJENGBROADCAST_INSERTION_LOCK:
-        broadcast_group = SESSION.query(KanjengBroadcast).get(
-            (keywoard, str(group_id))
-        )
+        broadcast_group = SESSION.query(KanjengBroadcast).get((keywoard, str(group_id)))
         return bool(broadcast_group)
 
 
