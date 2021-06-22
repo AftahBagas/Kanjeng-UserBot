4cacda8d447a522d948a199ac5c23fd5737ff7dd
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-06-23 06:13:13 +0700

    Update tools.py

--- pyPetercord/helpers/utils/tools.py
+++ pyPetercord/helpers/utils/tools.py
@@ -26,51 +26,51 @@ async def media_to_pic(event, reply, noedits=False):
     ]:
         return event, None
     if not noedits:
-        petercordevent = await edit_or_reply(
+        kanjengevent = await edit_or_reply(
             event, f"`Transfiguration Time! Converting to ....`"
         )
     else:
-        petercordevent = event
-    petercordmedia = None
-    petercordfile = os.path.join("./temp/", "meme.png")
-    if os.path.exists(petercordfile):
-        os.remove(petercordfile)
+        kanjengevent = event
+    kanjengmedia = None
+    kanjengfile = os.path.join("./temp/", "meme.png")
+    if os.path.exists(kanjengfile):
+        os.remove(kanjengfile)
     if mediatype == "Photo":
-        petercordmedia = await reply.download_media(file="./temp")
+        kanjengmedia = await reply.download_media(file="./temp")
         im = Image.open(petercordmedia)
-        im.save(petercordfile)
+        im.save(kanjengfile)
     elif mediatype in ["Audio", "Voice"]:
         await event.client.download_media(reply, petercordfile, thumb=-1)
     elif mediatype == "Sticker":
-        petercordmedia = await reply.download_media(file="./temp")
-        if petercordmedia.endswith(".tgs"):
+        kanjengmedia = await reply.download_media(file="./temp")
+        if kanjengmedia.endswith(".tgs"):
             await runcmd(
                 f"lottie_convert.py --frame 0 -if lottie -of png '{petercordmedia}' '{petercordfile}'"
             )
-        elif petercordmedia.endswith(".webp"):
-            im = Image.open(petercordmedia)
+        elif kanjengmedia.endswith(".webp"):
+            im = Image.open(kanjengmedia)
             im.save(petercordfile)
     elif mediatype in ["Round Video", "Video", "Gif"]:
         await event.client.download_media(reply, petercordfile, thumb=-1)
-        if not os.path.exists(petercordfile):
-            petercordmedia = await reply.download_media(file="./temp")
+        if not os.path.exists(kanjengfile):
+            kanjengmedia = await reply.download_media(file="./temp")
             clip = VideoFileClip(media)
             try:
-                clip = clip.save_frame(petercordfile, 0.1)
+                clip = clip.save_frame(kanjengfile, 0.1)
             except:
-                clip = clip.save_frame(petercordfile, 0)
+                clip = clip.save_frame(kanjengfile, 0)
     elif mediatype == "Document":
         mimetype = reply.document.mime_type
         mtype = mimetype.split("/")
         if mtype[0].lower() == "image":
-            petercordmedia = await reply.download_media(file="./temp")
-            im = Image.open(petercordmedia)
-            im.save(petercordfile)
-    if petercordmedia and os.path.exists(petercordmedia):
-        os.remove(petercordmedia)
-    if os.path.exists(petercordfile):
-        return petercordevent, petercordfile, mediatype
-    return petercordevent, None
+            kanjengmedia = await reply.download_media(file="./temp")
+            im = Image.open(kanjengmedia)
+            im.save(kanjengfile)
+    if kanjengmedia and os.path.exists(petercordmedia):
+        os.remove(kanjengmedia)
+    if os.path.exists(kanjengfile):
+        return kanjengevent, kanjengfile, mediatype
+    return kanjengevent, None
 
 
 async def take_screen_shot(
